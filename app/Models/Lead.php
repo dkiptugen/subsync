2510aff55dde404c6456cd48482fae24186905cd
Dennis Kiptugen <dennis.kiptugen@radioafricagroup.co.ke>
2026-07-15 10:46:47 +0300

    fdffd

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -4,17 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
-
 
 class Lead extends Model
+{
+    use HasFactory;
+
+    public function product()
     {
-        use HasFactory;
-
-
-        public function product()
-            {
-               return $this->belongsTo(Product::class) ;
-            }
+        return $this->belongsTo(Product::class);
     }
+}
--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -1,51 +1,52 @@
 <?php
 
-    namespace App\Models;
+namespace App\Models;
 
-    use App\Casts\TrimWhiteSpaceCast;
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
-    use Spatie\Activitylog\LogOptions;
-    use Spatie\Activitylog\Traits\LogsActivity;
+use App\Casts\TrimWhiteSpaceCast;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Models\Concerns\LogsActivity;
+use Spatie\Activitylog\Support\LogOptions;
 
-    class Organization extends Model
-        {
-            use HasFactory;
+class Organization extends Model
+{
+    use HasFactory;
 
-            protected $casts    = [
-                                        'name'            => TrimWhiteSpaceCast::class,
-                                        'registration_no' => TrimWhiteSpaceCast::class
-                                    ];
-            protected $fillable = [
-                                    'kra_pin',
-                                    'registration_no',
-                                    'name',
-                                    'address',
-                                    'phone_number',
-                                    'status',
-                                    'additional_items',
-                                    'user_id'
-                                ];
-            use LogsActivity;
+    protected $casts = [
+        'name' => TrimWhiteSpaceCast::class,
+        'registration_no' => TrimWhiteSpaceCast::class,
+    ];
 
-            public function getActivitylogOptions()
-            : LogOptions
-                {
-                    return LogOptions::defaults();
-                }
+    protected $fillable = [
+        'kra_pin',
+        'registration_no',
+        'name',
+        'address',
+        'phone_number',
+        'status',
+        'additional_items',
+        'user_id',
+    ];
 
-            public function user()
-                {
-                    return $this->belongsTo(User::class);
-                }
+    use LogsActivity;
 
-            public function users()
-                {
-                    return $this->hasMany(User::class);
-                }
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults();
+    }
 
-            public function whitelist()
-                {
-                    return $this->morphMany(UserWhitelist::class, 'whitelistable');
-                }
-        }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function whitelist()
+    {
+        return $this->morphMany(UserWhitelist::class, 'whitelistable');
+    }
+}
